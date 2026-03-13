5154e7d26c3d8fd3d358df340e5a23b00cd39e19
Nicolas Mastroviti <nmastroviti@gmail.com>
2026-03-13 12:23:03 -0300

    chore: add prettier, commitlint, husky, and lint-staged for code quality enforcement

---
+++ commitlint.config.cjs
@@ -0,0 +1,12 @@
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-case': [2, 'always', 'lower-case'],
+    'type-empty': [2, 'never'],
+    'type-enum': [
+      2,
+      'always',
+      ['build', 'chore', 'ci', 'docs', 'feat', 'fix', 'perf', 'refactor', 'revert', 'style', 'test'],
+    ],
+  },
+}
---
+++ lint-staged.config.cjs
@@ -0,0 +1,4 @@
+module.exports = {
+  '**/*.{ts,tsx,js,jsx}': ['eslint --fix --max-warnings=0', 'prettier --write'],
+  '**/*.{json,md,yml,yaml}': ['prettier --write'],
+}
