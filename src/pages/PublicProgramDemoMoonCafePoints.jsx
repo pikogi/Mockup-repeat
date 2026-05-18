f931569a92c41f666506911662b7fadc757aaea1
pikogi <giaoliva1@gmail.com>
2026-05-17 21:13:17 -0300

    fix: replace store image with portrait version for mooncafe-points demo

--- src/pages/PublicProgramDemoMoonCafePoints.jsx
+++ src/pages/PublicProgramDemoMoonCafePoints.jsx
@@ -79,8 +79,8 @@ function StoreScene({ onNext }) {
           inset: 0,
           width: '100%',
           height: '100%',
-          objectFit: 'contain',
-          objectPosition: 'center center',
+          objectFit: 'cover',
+          objectPosition: 'center top',
         }}
       />
 
